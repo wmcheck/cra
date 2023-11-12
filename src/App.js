7997a31555bebe64ec3b317ed68eed5f481b6592
Alexey Ilyasov <2194742@gmail.com>
2023-11-12 14:01:47 +0300

    upd App test ul

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">First CRA from GitHub Pages public</header>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
     </div>
   );
 }
