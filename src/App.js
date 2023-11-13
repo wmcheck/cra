13736c0ea169cf2bbb9e7ac2050f26425fdd28c4
Alexey Ilyasov <2194742@gmail.com>
2023-11-13 21:17:02 +0300

    feath: upd version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,94 @@
-import './App.css';
+import {
+  ThemeProvider,
+  BaseStyles,
+  Header,
+  Button,
+  Avatar,
+  Octicon,
+  Pagehead,
+  Box,
+  ProgressBar,
+  TabNav,
+  UnderlineNav,
+  Timeline,
+  StateLabel,
+  Breadcrumbs,
+} from '@primer/react';
+
+import { PageHeader, Blankslate } from '@primer/react/drafts';
+import {
+  MarkGithubIcon,
+  GitCommitIcon,
+  BookIcon,
+} from '@primer/octicons-react';
+
+const Line = () => (
+  <UnderlineNav aria-label="Main">
+    <UnderlineNav.Link href="#home" selected>
+      Home
+    </UnderlineNav.Link>
+    <UnderlineNav.Link href="#documentation">Documentation</UnderlineNav.Link>
+    <UnderlineNav.Link href="#support">Support</UnderlineNav.Link>
+  </UnderlineNav>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">First CRA from GitHub Pages public</header>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-      </ul>
-    </div>
+    <ThemeProvider>
+      <BaseStyles>
+        <Header>
+          <Header.Item>
+            <Header.Link
+              href="#"
+              sx={{
+                fontSize: 2,
+              }}
+            >
+              <Octicon
+                icon={MarkGithubIcon}
+                size={32}
+                sx={{
+                  mr: 2,
+                }}
+              />
+              <span>GitHub</span>
+            </Header.Link>
+          </Header.Item>
+          <Header.Item full>Menu</Header.Item>
+          <Header.Item
+            sx={{
+              mr: 0,
+            }}
+          >
+            <Avatar
+              src="https://github.com/octocat.png"
+              size={20}
+              square
+              alt="@octocat"
+            />
+          </Header.Item>
+        </Header>
+        <Box
+          sx={{
+            padding: 3,
+          }}
+        >
+          <PageHeader>
+            <PageHeader.TitleArea>
+              <PageHeader.Title>Title</PageHeader.Title>
+            </PageHeader.TitleArea>
+          </PageHeader>
+
+          <Breadcrumbs>
+            <Breadcrumbs.Item href="#">Home</Breadcrumbs.Item>
+            <Breadcrumbs.Item href="#">About</Breadcrumbs.Item>
+            <Breadcrumbs.Item href="#" selected>
+              Team
+            </Breadcrumbs.Item>
+          </Breadcrumbs>
+        </Box>
+      </BaseStyles>
+    </ThemeProvider>
   );
 }
-
 export default App;
